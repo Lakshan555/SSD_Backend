8c58d6a6f62b2242afe19bbaabc63bcf8a28ab73
pasindubhasura <srmpasindubhasura@gmail.com>
2022-11-07 11:51:26 +0530

    .

--- index.js
+++ index.js
@@ -10,22 +10,16 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 //configuring dotenv variables
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = "mongodb+srv://admin:admin123@cluster0.mfhcgcz.mongodb.net/?retryWrites=true&w=majority";
 
 //creating express server
 app.listen(PORT, async () => {
-    //mongoDB connection
-    try {
-      await mongoose.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-      });
-      console.log("MongoDB connected!🔥");
-    } catch (error) {
-      console.log(error);
+  //mongoDB connection
+  mongoose.connect(MONGO_URI, err => {
+    if (err) {
+      return console.error(err);
     }
-  
-    console.log(`Express server running at PORT ${PORT} 😍`);
+    console.log("MongoDB connected!🔥");
   });
+  console.log(`Express server running at PORT ${PORT} 😍`);
+});
