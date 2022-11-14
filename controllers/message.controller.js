237db669dbdced7a1589b4d669b1fa229f7d3775
Charith Widanapathirana <wcharithbuddhipriya@gmail.com>
2022-11-15 00:17:53 +0530

    update message controller

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -6,61 +6,68 @@ exports.addMessage = async (req, res) => {
     const { staffId, message } = req.body;
     try {
 
-        const file = req.files.uploadFile;
+        const file = req.files ? req.files.uploadFile : '';
         const path = `uploads/${staffId}`;
         const correctPath = `${path}/` + file.name;
 
-        if (fs.existsSync(correctPath)) {
-            res.status(409).json('File is already exist!');
-        } else {
+        if (file) {
+            console.log('File have')
 
-            if (req.files && message) {
+            if (fs.existsSync(correctPath)) {
+                res.status(409).json('File is already exist!');
+            } else {
 
-                if (!fs.existsSync(path)) {
+                if (req.files && message) {
 
-                    fs.mkdirSync(path);
-                    console.log('path created');
-                    console.log(`file path , ${path}/` + file.name);
-                }
+                    if (!fs.existsSync(path)) {
+                        //path not exist create new path
+                        fs.mkdirSync(path);
+                        console.log('path created');
+                        console.log(`file path , ${path}/` + file.name);
+                    }
 
-                console.log('move to path');
-                console.log(`file path , ${path}/` + file.name);
-
-                //send message and upload file to server
-                const newMessage = new Message({ staffId, message, uploadFile: correctPath });
-                file.mv(`${path}/` + file.name, function (err, result) {
-                    if (err) { throw err; }
-                    console.log('Correct Path ', correctPath);
-                    newMessage.save();
-                    res.status(201).json('Message with file saved successfully');
-                });
-                
-            } else if (req.files) {
-
-                if (fs.existsSync(path)) {
                     console.log('move to path');
                     console.log(`file path , ${path}/` + file.name);
+
+                    //send message and upload file to server
+                    const newMessage = new Message({ staffId, message, uploadFile: correctPath });
+                    file.mv(`${path}/` + file.name, function (err, result) {
+                        if (err) { throw err; }
+                        console.log('Correct Path ', correctPath);
+                        newMessage.save();
+                        res.status(201).json('Message with file saved successfully');
+                    });
+
                 } else {
-                    fs.mkdirSync(path);
-                    console.log('path created');
-                    console.log(`file path , ${path}/` + file.name);
+
+                    if (fs.existsSync(path)) {
+                        console.log('move to path');
+                        console.log(`file path , ${path}/` + file.name);
+                    } else {
+                        fs.mkdirSync(path);
+                        console.log('path created');
+                        console.log(`file path , ${path}/` + file.name);
+                    }
+
+                    //set upload location path with file name
+                    const newMessage = new Message({ staffId, uploadFile: correctPath });
+                    file.mv(`${path}/` + file.name, function (err, result) {
+                        if (err) { throw err; }
+                        console.log('File only ', newMessage)
+                        newMessage.save();
+                        res.status(201).json('File uploaded successfully!');
+                    });
                 }
-
-                //set upload location path with file name
-                const newMessage = new Message({ staffId, uploadFile: correctPath });
-                file.mv(`${path}/` + file.name, function (err, result) {
-                    if (err) { throw err; }
-                    console.log('File only ', newMessage)
-                    newMessage.save();
-                    res.status(201).json('File uploaded successfully!');
-                });
-
-            } else {
-                const newMessage = new Message({ staffId, message });
-                newMessage.save()
-                res.status(201).json('Message saved successfully');
             }
+
+
+        } else {
+            console.log('No File ')
+            const newMessage = new Message({ staffId, message });
+            newMessage.save()
+            res.status(201).json('Message saved successfully');
         }
+
 
     } catch (error) {
         res.status(500).json(error);
